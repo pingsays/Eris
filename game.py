2236c4e416f25a6503c357c9a24f2e6b0ca711de
Ping Lin <pingli88@gmail.com>
2019-10-19 00:19:13 -0400

    feat: load cards from yaml and shuffle the decks when initializing game

--- game.py
+++ game.py
@@ -1,3 +1,41 @@
+from pprint import pprint
+import yaml
+import random
+
+cards = 'cards.yaml'
+
+
 class Game():
     def __init__(self):
-        pass
+        self.door = []
+        self.treasure = []
+
+        # load in the cards from yaml file
+        with open(cards, 'r') as c:
+            self.cards = yaml.load(c)
+
+        # put all door cards into the door list variable
+        for i in self.cards['door']:
+            for j in self.cards['door'][i]:
+                self.door.append(j)
+
+        # put all door cards into the door list variable
+        for i in self.cards['treasure']:
+            for j in self.cards['treasure'][i]:
+                self.treasure.append(j)
+
+        # shuffle the decks
+        random.shuffle(self.door)
+        random.shuffle(self.treasure)
+
+
+class Player():
+    def __init__(self):
+        self.hand = []
+        self.field = []
+
+
+x = Game()
+pprint(x.door)
+print()
+pprint(x.treasure)
