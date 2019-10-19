166156abdefcca8d8a49487a5ada2b59a82bd355
Ping Lin <pingli88@gmail.com>
2019-10-19 09:42:11 -0400

    feat: added a player method to draw cards and check what is in the hand

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ from pprint import pprint
 import yaml
 import random
 
-cards = 'cards.yaml'
+cards = 'cards2.yaml'
 
 
 class Game():
@@ -16,8 +16,7 @@ class Game():
 
         # put all door cards into the door list variable
         for i in self.cards['door']:
-            for j in self.cards['door'][i]:
-                self.door.append(j)
+            self.door.append(i)
 
         # put all door cards into the door list variable
         for i in self.cards['treasure']:
@@ -39,10 +38,34 @@ class Player():
         self.hand = []
         self.field = []
 
+    def draw_card(self, action, deck):
+        reveal_card = deck.pop(0)  # need to rename variable to something that makes more sense
+
+        if action == 'kick_open_the_door':
+            if reveal_card['card_type'] == 'curse':
+                print(f"Curse: {reveal_card['name']}")
+                print(f"Description: {reveal_card['description']}", end='')
+            elif reveal_card['card_type'] == 'monster':
+                print(f"Monster Name: {reveal_card['name']}")
+                print(f"Ability: {reveal_card['description']['ability']}", end='')
+                print(f"Bad Stuff: {reveal_card['description']['bad_stuff']}", end='')
+        else:  # this includes loot the room and monster treasures
+            self.hand.append(reveal_card)
+
+    def check_hand(self):
+        for card in self.hand:
+            for key, value in card.items():
+                print(key, value)
+
 
 x = Game()
 pprint(x.door)
 print()
 pprint(x.treasure)
+print()
 
-x.roll_dice()
+ping = Player()
+ping.draw_card('kick_open_the_door', x.door)
+# ping.draw_card('loot_the_room', x.door)
+# print(ping.field[0]['name'])
+# ping.check_hand()
