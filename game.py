0ddc06238f7c42f081aa565dd4339e53a5a1baff
Ping Lin <pingli88@gmail.com>
2019-10-23 20:42:17 -0400

    changed item type in Player().hand from list to dict and added use_card method

--- game.py
+++ game.py
@@ -53,24 +53,42 @@ class Game():
 class Player():
     def __init__(self):
         self.hand = []
-        self.field = []
+        self.playing_field = {
+            'temp': [], 'equipment': [], 'monster': [], 'curse': [], 'class': [], 'race': []
+        }
 
     def draw_card(self, action, deck):
         card_details = deck.pop(0)  # need to rename variable to something that makes more sense
-        card = []
+        card = {}
 
-        for key, value in card_details.items():
-            if key == 'description':
-                for i in value:
-                    card.append(f"{i}")
-            elif key == 'metadata':
-                continue
-            else:
-                card.append(f"{key}: {value}")
-        self.hand.append(card)
+        # for key, value in card_details.items():
+        #     if key == 'description':
+        #         for i in value:
+        #             card.append(f"{i}")
+        #     elif key == 'metadata':
+        #         continue
+        #     else:
+        #         card.append(f"{key}: {value}")
+        # self.hand.append(card)
 
-    def discard_card(self, card_index, target):
-        target.append(self.hand.pop(card_index))
+        self.hand.append(card_details)
+
+    def use_card(self, card_index, target):
+        """this can be used for both playing or discarding"""
+
+        card = self.hand.pop(card_index)
+
+        # check if the target is a Player class
+        if isinstance(target, Player):
+            if 'monster' in card.keys():
+                target['playing_field']['monster'].append(card)
+        else:
+            target.append(self.hand.pop(card_index))
+
+    # def sort_card(self, card, target):
+    #     """this method should be called whenever a card is used on player"""
+    #     if 'monster' in card.keys():
+    #         target['playing_field']['monster'].append(card)
 
     def check_hand(self):
         for card in self.hand:
@@ -100,33 +118,53 @@ for i in range(4):
     for player in players.values():
         player.draw_card('game_setup', mk.treasure_draw)
 
+#=====================#
+# testing player hand #
+#=====================#
+for player in player_names:
+    header_row = f"-- {player}'s hand --"
+    header_len = len(header_row)
 
-# for player in player_names:
-#     header_row = f"-- {player}'s hand --"
-#     header_len = len(header_row)
+    print("-" * header_len)
+    print(header_row)
+    print("-" * header_len)
 
-#     print("-" * header_len)
-#     print(header_row)
-#     print("-" * header_len)
+    for card in players[player].hand:
+        for key, value in card.items():
+            if 'metadata' in key:
+                continue
+            elif 'description' in key:
+                print('description: ', end='')
+                for i in value:
+                    print(i)
+            else:
+                print(f"{key}: {value}")
+        print()
 
-#     for card in players[player].hand:
-#         for i in card:
-#             print(i)
-#         print()
-
+#===========================================#
+# testing discard card and reshuffling deck #
+#===========================================#
 print(players['ping'].hand)
-players['ping'].discard_card(0, mk.door_discard)
-players['ping'].discard_card(0, mk.door_discard)
-players['ping'].discard_card(0, mk.door_discard)
-players['ping'].discard_card(0, mk.door_discard)
-print()
-print(mk.door_discard)
-print()
-# print(players['ping'].hand)
-print()
+# players['ping'].discard_card(0, mk.door_discard)
+# players['ping'].discard_card(0, mk.door_discard)
+# players['ping'].discard_card(0, mk.door_discard)
+# players['ping'].discard_card(0, mk.door_discard)
+# print()
+# print(mk.door_discard)
+# print()
+# # print(players['ping'].hand)
+# print()
 
-print('reshuffling')
-mk.reshuffle_discard_pile('door')
-print(f"discard: {mk.door_discard}")
-print()
-print(f"draw: {mk.door_draw}")
+# print('reshuffling')
+# mk.reshuffle_discard_pile('door')
+# print(f"discard: {mk.door_discard}")
+# print()
+# print(f"draw: {mk.door_draw}")
+
+# print(type(players['ping']))
+
+# print(isinstance(players['ping'], Player))
+# if type(players['ping']) == :
+#     print('true')
+# else:
+#     print('false')
